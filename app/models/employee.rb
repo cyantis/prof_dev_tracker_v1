1c91253d8f6c44f548470f3b53d379381952904c
cyantis <cyantis@gmail.com>
2019-10-05 10:40:05 -0600

    adds bcrypt & nested route for employees-> events

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,9 @@ class Employee < ApplicationRecord
   belongs_to :location
   has_many :employees_events
   has_many :events, through: :employees_events
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :events
-  resources :employees
+  resources :employees do
+    resources :events, only: [:new, :create]
+  end
   resources :locations
 end
