cb3c1ae72074fa5875ce2379604062288344047f
cyantis <cyantis@gmail.com>
2019-10-07 10:55:38 -0600

    adds event link functionality to index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,7 +19,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @employee = Employee.find(params[:employee_id])
+    @employee = Employee.find(params[:employee_id]) if params[:employee_id]
   end
 
   def edit
