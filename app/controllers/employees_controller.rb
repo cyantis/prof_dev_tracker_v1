20a151e0972f3f67b9f88a08ce9b5ae69cfcd2ff
cyantis <cyantis@gmail.com>
2019-10-07 15:46:24 -0600

    adds account creation functionality

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,10 +1,26 @@
 class EmployeesController < ApplicationController
 
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+
+    if @employee.save
+      flash[:message] = "Employee Created!"
+      redirect_to employee_path(@employee)
+    else
+      flash[:message] = "All fields are required. Please, enter again!"
+      render :new
+    end
+  end
+
   def show
     @employee = Employee.find(params[:id])
   end
 
   def employee_params
-    params.require(:employee).permit(:id, :username, :password, :email, :name, :title, :bio, :location_id, event_ids: [])
+    params.require(:employee).permit(:id, :username, :password, :email, :name, :title, :bio, :location_id, :manager_id, event_ids: [])
   end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
   def event_type_arr
-    Event.event_order.collect {|e| e.category}
+    Event.event_order.collect { |e| e.category }
   end
 
 end
